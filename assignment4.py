155bbd86bdd3194319936fce5e08bec99ef869f6
Neal Rogers <neal.rogers@spsmail.cuny.edu>
2017-02-21 13:40:46 -0500

    Initial commit with required functions added.

---
+++ assignment4.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+""" This program does stuff.
+"""
+
+
+def sequential_search():
+def ordered_sequential_search():
+def binary_search_iterative():
+def binary_search_recursive():
+
+
+def main():
+    print
+
+
+if __name__ == '__main__':
+    main()
